845f9223894b32c71b753d7bba5b6d986ad2baa9
Mihaela Pavleska <mihaela.pavleska@gmail.com>
2022-12-21 04:02:55 +0100

    lab 2 do 7mo

--- src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
@@ -1,11 +1,13 @@
 package mk.ukim.finki.wp.lab.bootstrap;
 
 import mk.ukim.finki.wp.lab.model.Balloon;
+import mk.ukim.finki.wp.lab.model.Manufacturer;
 import mk.ukim.finki.wp.lab.model.Order;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -14,20 +16,27 @@ public class DataHolder {
     public static List<Balloon> balloons = new ArrayList<>();
     public static List<Balloon> balloonSizes = new ArrayList<>();
     public static List<Order> orders = new ArrayList<>();
+    public static List<Manufacturer> manufacturers = new ArrayList<>();
 
     @PostConstruct
     public void init() {
-        balloons.add(new Balloon("Red", "Red Balloon"));
-        balloons.add(new Balloon("Blue", "Blue Balloon"));
-        balloons.add(new Balloon("Green", "Green Balloon"));
-        balloons.add(new Balloon("Yellow", "Yellow Balloon"));
-        balloons.add(new Balloon("Black", "Black Balloon"));
+        manufacturers.add(new Manufacturer("Manufacturer 1", "MKD", "Kiro Pajmakoski, 9"));
+        manufacturers.add(new Manufacturer("Manufacturer 2", "SRB", "Left Street Up, 81A"));
+        manufacturers.add(new Manufacturer("Manufacturer 3", "GER", "Razlovecko Vostanie, 5"));
+        manufacturers.add(new Manufacturer("Manufacturer 4", "CRO", "Non-Fake Street, 436"));
+        manufacturers.add(new Manufacturer("Manufacturer 5", "BUG", "Fun Street, 1"));
 
-        balloonSizes.add(new Balloon("Big", "Big Balloon"));
-        balloonSizes.add(new Balloon("Small", "Small Balloon"));
-        balloonSizes.add(new Balloon("Tall", "Tall Balloon"));
-        balloonSizes.add(new Balloon("Short", "Short Balloon"));
-        balloonSizes.add(new Balloon("Wide", "Wide Balloon"));
+        balloons.add(new Balloon("Red", "Red Balloon", manufacturers.get(0)));
+        balloons.add(new Balloon("Blue", "Blue Balloon", manufacturers.get(1)));
+        balloons.add(new Balloon("Green", "Green Balloon", manufacturers.get(2)));
+        balloons.add(new Balloon("Yellow", "Yellow Balloon", manufacturers.get(3)));
+        balloons.add(new Balloon("Black", "Black Balloon", manufacturers.get(4)));
+
+        balloonSizes.add(new Balloon("Big", "Big Balloon", manufacturers.get(0)));
+        balloonSizes.add(new Balloon("Small", "Small Balloon", manufacturers.get(1)));
+        balloonSizes.add(new Balloon("Tall", "Tall Balloon", manufacturers.get(2)));
+        balloonSizes.add(new Balloon("Short", "Short Balloon", manufacturers.get(3)));
+        balloonSizes.add(new Balloon("Wide", "Wide Balloon", manufacturers.get(4)));
     }
 
 
